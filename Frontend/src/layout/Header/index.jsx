2a05db863a348444114b6699249c325c9250874a
“yourGithubUsername” <your@emailadd.com>
2021-12-03 12:03:16 +0000

    add navbar

--- Frontend/src/layout/Header/index.jsx
+++ Frontend/src/layout/Header/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './style.css';
 
 export function Header() {
     return(
         <>
-            <h1>Header goes here</h1>
+            
+            <nav>
+                <p>LOGO (ptag for now)</p>
+                <NavLink exact to="/">Front</NavLink>
+                <NavLink to="/MainFeed">Feed</NavLink>
+                <NavLink to="/Profile">Profile</NavLink>
+                <NavLink to="/Categories">Groups</NavLink>
+
+            </nav>
+            
         </>
     )
 }
