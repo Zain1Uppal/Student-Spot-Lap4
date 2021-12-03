c39c413d7ac4ceb9f1a44664e683f4c41424e4da
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-12-03 16:03:33 +0000

    set followed fields to null initially

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,8 +4,8 @@ from categories.models import Category
 
 # Create your models here.
 class User(AbstractUser):
-    followed_users = models.ManyToManyField("self")
-    followed_categories = models.ManyToManyField(Category)
+    followed_users = models.ManyToManyField("self", null=True)
+    followed_categories = models.ManyToManyField(Category, null=True)
     # Pre-defined images
     # profile_pic = 
 
