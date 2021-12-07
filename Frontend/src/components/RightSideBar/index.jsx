2f35ead2e5a3102ab11bfd567a053cb0f1ead5e2
“yourGithubUsername” <your@emailadd.com>
2021-12-07 11:54:02 +0000

    add JS for username RSB

--- Frontend/src/components/RightSideBar/index.jsx
+++ Frontend/src/components/RightSideBar/index.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
-import './style.css'
+import React, {useState, useEffect} from 'react';
+import './style.css';
 
 export function RightSideBar() {
+
+    const [ username, setUsername ] = useState("");
+
+    useEffect(() => {
+        if (!localStorage.getItem('token')) {
+          window.location.replace('http://localhost:8080/login');
+        } else {
+          fetch('http://localhost:8000/users/auth/user/', {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Token ${localStorage.getItem('token')}`
+            }
+          })
+            .then(res => {if(res.status === 200){
+                            return res.json()
+                          }else{
+                            localStorage.clear()
+                            window.location.replace('http://localhost:8080/login');
+                          }
+                        })
+            .then(data => {
+              setUsername(data.username);
+            });
+        }
+    }, []);
+
     return (
         <div className="right-side-bar">
             <div className="right-side-top">
                 <div className="rsb-pic-cont">
                     <img className="rsb-user-pic"src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/2048px-User_icon_2.svg.png" alt="rsb-friend-pic"/>
                 </div>
-                <div className="rst-username">Username</div>
+                <div className="rst-username">{username}</div>
             </div>
             <hr />
             <ul className="right-side-bottom">
