4b55dda607694e551bb1fe781e045a8eaf4bd555
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-12-07 13:09:21 +0000

    add update/delete routes for post w/ custom permissions

---
+++ backend/users/permissions.py
@@ -0,0 +1,32 @@
+from rest_framework import permissions
+
+class IsPosterOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of a post or admins to edit it
+    """
+    edit_methods = ("PUT", "PATCH")
+
+    def has_permission(self, request, view):
+        # Needs to be authenticated for basic access
+        if request.user.is_authenticated:
+            return True
+
+    def has_object_permission(self, request, view, obj):
+        # Admin users have full access
+        if request.user.is_superuser:
+            return True
+
+        # Anyone can access GET, OPTIONS, HEAD methods
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Posters can edit/delete their posts
+        if obj.poster == request.user:
+            return True
+
+        # Staff users can delete posts
+        if request.user.is_staff and request.method not in self.edit_methods:
+            return True
+        
+        # Otherwise deny access
+        return False
