ebc5ad8290032709c6d70734a35edf8cfba6268c
Zain Uppal <zainuppal1@gmail.com>
2021-12-07 11:46:56 +0000

    add show posts

--- Frontend/src/components/Post/index.jsx
+++ Frontend/src/components/Post/index.jsx
@@ -2,7 +2,7 @@ import React, {useState,useEffect} from 'react';
 import './style.css'
 
 export function Post({userId}) {
-    const [postData, setPostData] = useState()
+    const [postData, setPostData] = useState([])
     useEffect(()=>{
         console.log(userId)
         fetch(`http://localhost:8000/posts/users/${userId}/following`, {
@@ -13,37 +13,47 @@ export function Post({userId}) {
             }
         })
         .then(res => res.json())
-        .then(data => setPostData(data))
+        .then(data => setPostData(data.data))
     },[])
-
-    return(
-        <div className="post-feed">
-            <div className="post-wrapper">
-                <div className="post-top">
-                    <div className="post-top-left">
-                        <img className="post-profile-pic" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/2048px-User_icon_2.svg.png" alt="user-pic"/>
-                        <span className="post-username">Username</span>
-                        <span className="post-date">5 mins ago</span>
+    
+    function dataPost(){
+        console.log('in the function'+ postData)
+        return(
+        postData.map((p, i) => 
+            <div className="post-feed" key={i}>
+                <div className="post-wrapper">
+                    <div className="post-top">
+                        <div className="post-top-left">
+                            <img className="post-profile-pic" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/2048px-User_icon_2.svg.png" alt="user-pic"/>
+                            <span className="post-username">Username</span>
+                            <span className="post-date">{p.date}</span>
+                        </div>
+                        <div className="post-top-right"></div>
+                        <i className="fas fa-caret-down"></i>
                     </div>
-                    <div className="post-top-right"></div>
-                    <i className="fas fa-caret-down"></i>
-                </div>
-                <div className="post-center">
-
-                    <span className="post-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium.</span>
-                </div>
-                <div className="post-bottom">
-                    <div className="post-bottom-left">
-                        <i className="fas fa-thumbs-up"></i>
-                        <i className="fas fa-thumbs-down"></i>
+                    <div className="post-center">
+                        <span className="post-text" >{p.body}</span>
                     </div>
-                    <div className="post-bottom-right">
-                        <div className="post-comment-text">Add a comment</div>
+                    <div className="post-bottom">
+                        <div className="post-bottom-left">
+                            <i className="fas fa-thumbs-up"></i>
+                            <i className="fas fa-thumbs-down"></i>
+                        </div>
+                        <div className="post-bottom-right">
+                            <div className="post-comment-text">Add a comment</div>
+                        </div>
                     </div>
-                    
                 </div>
             </div>
+            )
+        )
 
+        
+    }
+
+    return(
+        <div className='overall'>
+            {dataPost()}
         </div>
     )
 }
