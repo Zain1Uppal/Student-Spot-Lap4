03214f36a6574bfabfe19fc445025ad10d6839aa
Rach <90192629+Rachiey@users.noreply.github.com>
2021-12-07 12:31:46 +0000

    added users page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Footer } from './layout/index';
-import { MainFeed, Profile, Categories, Category, FrontPage } from './pages/index';
+import { MainFeed, Profile, Categories, Category, FrontPage, UserPage } from './pages/index';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './style.css'
 import Navbar from './layout/Navbar';
@@ -13,17 +13,22 @@ import { Dashboard } from './views/app/Dashboard'
 function App() {
     return (
         <>
+        
             <Switch>
+            
                 <Route exact path="/"><FrontPage/> <Navbar /> </Route>
                 <Route path='/login' component={Login} exact />
                 <Route path='/signup' component={Signup} exact />
                 <Route path='/logout' component={Logout} exact />
                 <Route path="/MainFeed"><MainFeed/></Route>
                 <Route path="/Profile"><Profile /></Route>
+                <Route path="/:username"><UserPage /></Route>
                 <Route path="/Categories"><Categories /></Route>
                 <Route path="/Category"><Category /></Route>
                 <Route path="/dashboard"><Dashboard /></Route>
+               
             </Switch>
+        
             {/* <Footer /> */}
         </>
 
--- Frontend/src/pages/index.js
+++ Frontend/src/pages/index.js
@@ -3,3 +3,4 @@ export { MainFeed } from "./MainFeed";
 export { default as Profile } from "./Profile";
 export { FrontPage } from "./FrontPage";
 export { Category } from "./Category"
+export { UserPage } from "./UserPage"
