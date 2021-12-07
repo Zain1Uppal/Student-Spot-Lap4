afa8dee9705c88eed30998d5a8e5946197be4b6a
Zain Uppal <zainuppal1@gmail.com>
2021-12-07 13:52:29 +0000

    remove conditional

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.db.models.fields.related import ManyToManyField
 from categories.models import Category
 from rest_framework.authtoken.models import Token
 
