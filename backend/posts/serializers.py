3bad9d7537333fec9da64c02fc4952415a18579c
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-12-08 10:19:00 +0000

    use username instead of user id in poster field

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import Post, Comment
+from users.models import User
 
 class PostSerializer(serializers.ModelSerializer):
+    poster = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='username')
     class Meta:
         model = Post
         fields = "__all__"
