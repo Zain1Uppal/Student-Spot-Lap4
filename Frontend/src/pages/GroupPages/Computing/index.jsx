fe4c4a69f17e131794dbf288e94225d6ce5531e3
“yourGithubUsername” <your@emailadd.com>
2021-12-08 13:18:52 +0000

    computing page setup

--- Frontend/src/pages/GroupPages/Computing/index.jsx
+++ Frontend/src/pages/GroupPages/Computing/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Header } from '../../../layout/index';
+import { LeftSideBar, GroupSection } from '../../../components/index';
 import './style.css';
 
 export function Computing(){
 
     return(
-        <>
-        </>
+        <div className="computing-page">
+        <Header />
+        <div className="content-computing">
+            <LeftSideBar />
+            <div className="group-computing">
+                <h1>Computing</h1>
+                <GroupSection />
+            </div>
+            
+            
+            
+            
+        </div>
+    </div> 
     )
 }
