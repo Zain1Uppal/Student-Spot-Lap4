0b841dccf7ab5b3286033f60725b866f78a58016
“yourGithubUsername” <your@emailadd.com>
2021-12-08 13:20:51 +0000

    english page setup

--- Frontend/src/pages/GroupPages/English/index.jsx
+++ Frontend/src/pages/GroupPages/English/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Header } from '../../../layout/index';
+import { LeftSideBar, GroupSection } from '../../../components/index';
 import './style.css';
 
 export function English(){
 
     return(
-        <>
-        </>
+        <div className="english-page">
+            <Header />
+            <div className="content-english">
+                <LeftSideBar />
+                <div className="group-english">
+                    <h1>English</h1>
+                    <GroupSection />
+                </div>
+                
+                
+                
+                
+            </div>
+        </div> 
     )
 }
