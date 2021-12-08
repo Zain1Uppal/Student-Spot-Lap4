38624e717ad4badf646e115b1c19afa197680c89
“yourGithubUsername” <your@emailadd.com>
2021-12-08 11:20:27 +0000

    update style createpost

--- Frontend/src/pages/Profile/index.jsx
+++ Frontend/src/pages/Profile/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, Fragment } from 'react';
+import { CreatePost, Post  } from '../../components/index';
 import './style.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header } from '../../layout/index';
@@ -42,81 +43,53 @@ export const Profile = () => {
       
     return (
        
-      <div>
+      <div className="pp-head">
          
         <Header />
         {loading === false && (  
         <div className="page-holder">
             <div className="contentDiv">
                   <div className="">
-                    <h1 className="page-heading">{userName}'s Profile</h1>
+                    <h1 className="page-heading1">{userName}'s Profile</h1>
                   </div>
               <section>
                 <div className="row">
-                  <div className="col-lg-4">
-                    <div className="card card-profile">
+                  <div className="col-lg-41">
+                    <div className="card1 card-profile1">
                       <div className="card-header" style={{backgroundImage: "url(https://pbs.twimg.com/media/EeI6u48WkAAC45D.jpg)"}}> </div>
-                      <div className="card-body text-center"> <img className="card-profile-img" src="https://i.pinimg.com/originals/d7/fd/9e/d7fd9e0b952d5f9b9adff6ec29a8b20d.png" alt="profile img"/>
-                        <h3 className="mb-3">{firstName}</h3>
+                      <div className="card-body1 text-center"> <img className="card-profile-img" src="https://i.pinimg.com/originals/d7/fd/9e/d7fd9e0b952d5f9b9adff6ec29a8b20d.png" alt="profile img"/>
+                        <h3 className="mb-3">{userName}</h3>
                         <p className="mb-4">University Course goes here</p>
                         <p className="mb-4">University here</p>
-                        <div className= "btn-toolbar">
-                        <button className="btn btn-outline-secondary" type="button"><i className="fa fa-paper-plane"></i> My Messages</button>
-                        </div>
+                        <p className="followers-pp">Followers:</p>
                       </div>
                     </div>
-                    <div className="card mb-4">
+                    
+                    <div className=" card-profile-div">
+                      
+                      <h4 className="card-heading">My Profile</h4>
+                      
                       <div className="card-body">
-                        <div className="d-flex align-items-center">
-                          <div className="flex-grow-1 ps-3">
-                            <h4>Follow me:</h4>
-                            <p className="text-muted mb-0">Social media links here</p>
-                            <ul className="social-links list-inline mb-0 mt-2">
-                              <li className="list-inline-item"><a href="https://www.google.com/" data-bs-toggle="#tooltip" data-placement="top" title="" data-bs-original-title="Bob's Facebook" aria-label="Bob's Facebook"><i className="fab fa-facebook"></i></a></li>
-                              <li className="list-inline-item"><a href="https://www.google.com/" data-bs-toggle="#tooltip" data-placement="top" title="" data-bs-original-title="@bob" aria-label="@bob"><i className="fab fa-twitter"></i></a></li>
-                              <li className="list-inline-item"><a href="https://www.google.com/" data-bs-toggle="#tooltip" data-placement="top" title="" data-bs-original-title="@bob" aria-label="@bob"><i className="fab fa-instagram"></i></a></li>
-                              <li className="list-inline-item"><a href="https://www.google.com/" data-bs-toggle="#tooltip" data-placement="top" title="" data-bs-original-title="@nathan" aria-label="@bob"><i className="fab fa-skype"></i></a></li>
-                            </ul>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <form className="card mb-4">
-                      <div className="card-header">
-                        <h4 className="card-heading">My Profile</h4>
-                      </div>
-                      <div className="card-body">
-                        <div className="row mb-3">
-                          <div className="col-auto d-flex align-items-center"><img className="avatar avatar-lg p-1" src="https://i.pinimg.com/originals/d7/fd/9e/d7fd9e0b952d5f9b9adff6ec29a8b20d.png" alt="Avatar"/></div>
-                          <div className="col">
-                            <label className="form-label">Name</label>
-                            <input className="form-control" placeholder={firstName}/>
-                          </div>
-                        </div>
-                        <div className="mb-3"> 
-                          <label className="form-label">Bio</label>
-                          <textarea className="form-control" rows="8">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</textarea>
-                        </div>
-                        {/* can be added when changing password/email */}
-                        {/* <div className="mb-3"> 
-                          <label className="form-label">Email</label>
-                          <input className="form-control" placeholder="you@domain.com"/>
-                        </div>
-                        <label className="form-label">Password</label>
-                        <input className="form-control" type="password" value="password" />
-                      </div>
-                      <div className="card-footer text-end">
-                        <button className="btn btn-primary">Save</button>
-                      </div> */}
+                        <h4 className="user-bio-pp">User bio goes here</h4>
+                        
+                        
 
                       </div>
-                    </form>
+                    </div>
                   </div>
                 </div>
               </section>
             </div>
             <div className="col-lg-8">
-              <h1>hi, is this connected?</h1>
+              <div className="create-p-cont">
+                <CreatePost />
+              </div>
+              <div className="pp-post-cont">
+                ALL THE POSTS GOES BELOW. DELETE THIS TEXT ONCE ALL IS CONNECTED
+                <Post />
+                
+              </div>
+              
             </div>
           
           </div>
