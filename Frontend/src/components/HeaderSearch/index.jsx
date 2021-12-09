127eeb90b3f0f91bf97df039f3bc1eaa0cc7d25b
Zain Uppal <zainuppal1@gmail.com>
2021-12-09 16:37:07 +0000

    fix redirect

--- Frontend/src/components/HeaderSearch/index.jsx
+++ Frontend/src/components/HeaderSearch/index.jsx
@@ -1,22 +1,16 @@
 import React, {useState} from 'react';
 import './style.css';
 
-
-export const HeaderSearch = (data) => {
+export const HeaderSearch = () => {
     const [search, setSearch] = useState()
-
     function searchForUser(){
         window.location.replace(`http://localhost:8080/users/${search}`)
-
     }
-    console.log(search)
-
     return(
         <>
             <div className="wrapper">
                 <div className="search-input">
                   <a href="" target="_blank" hidden></a>
-
                    <input type="text" placeholder="Search a user" onChange={e=>setSearch(e.target.value)} ></input>
                     <div className="icon"><button type='submit' className="fas fa-search" onClick={searchForUser}></button></div>
                   <div className="autocom-box"></div>
