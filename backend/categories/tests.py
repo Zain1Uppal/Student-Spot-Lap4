f3551cbacc504c2419ba0ff6470a28eb4517d9c8
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-12-09 13:06:49 +0000

    complete categories test suite

--- backend/categories/tests.py
+++ backend/categories/tests.py
@@ -1,4 +1,5 @@
 from logging import log
+from django import test
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIClient
@@ -10,11 +11,13 @@ from .models import Category
 class BaseTestCase(TestCase):
 
     @classmethod
-    def setupTestData(cls):
+    def setUpTestData(cls):
         cls.category = Category.objects.create(name="Test Category")
         cls.user = User.objects.create_user("Pingu", "pingu@nootnoot.com", "nootnoot")
-        cls.user.is_superuser = True
-        cls.user.save()
+        cls.superuser = User.objects.create_superuser("Bob", "example@example.com", "example123")
+
+    def test_category_str(self):
+        self.assertEqual(str(Category.objects.get()), self.category.name)
 
 class TestUnauthorisedViews(BaseTestCase):
     c = APIClient()
@@ -41,19 +44,37 @@ class TestAuthorisedViews(BaseTestCase):
         self.c = APIClient()
         self.c.login(username="Pingu", password="nootnoot")
 
-    def test_index(self):
+    def test_auth_index(self):
         response = self.c.get(reverse('categories-index'))
         self.assertEqual(response.status_code, 200)
-        resp_data = response.data
-        self.assertEqual(resp_data.count(), 1)
+        resp_data = response.data["data"]
+        self.assertEqual(len(resp_data), 1)
 
-    # def test_show(self):
-    #     response = self.c.get(reverse('category-by-id', kwargs={"cateId": 1}))
-    #     self.assertEqual(response.status_code, 401)
-    #     assert "Authentication credentials were not provided."
+    def test_auth_show(self):
+        response = self.c.get(reverse('category-by-id', kwargs={"cateId": 1}))
+        self.assertEqual(response.status_code, 200)
+        resp_data = response.data["data"]
+        self.assertEqual(resp_data, [{"id": self.category.id, "name": self.category.name}])
     
-    # def test_create(self):
-    #     test_data = {"name": "New Test Category"}
-    #     response = self.c.post(reverse('categories-create'), test_data, format="json")
-    #     self.assertEqual(response.status_code, 401)
-    #     assert "Authentication credentials were not provided."
+    def test_auth_create(self):
+        test_data = {"name": "New Test Category"}
+        response = self.c.post(reverse('categories-create'), test_data, format="json")
+        self.assertEqual(response.status_code, 403)
+
+class TestAdminViews(BaseTestCase):
+    
+    def setUp(self):
+        self.c = APIClient()
+        self.c.login(username="Bob", password="example123")
+
+    def test_admin_create(self):
+        test_data = {"name": "New Test Category"}
+        response = self.c.post(reverse('categories-create'), test_data, format="json")
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Category.objects.count(), 2)
+        self.assertEqual(response.data, {"id": 2, "name": test_data["name"]})
+
+    def test_failed_create(self):
+        test_data = {"body": "oops"}
+        response = self.c.post(reverse('categories-create'), test_data, format="json")
+        self.assertEqual(response.status_code, 400)
