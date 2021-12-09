883547e0a3d2e4521e2c8dd4483cbfc938039a2b
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-12-09 17:07:40 +0000

    add tests

--- backend/posts/tests.py
+++ backend/posts/tests.py
@@ -4,6 +4,7 @@ from rest_framework.test import APIClient
 from datetime import date
 
 from users.models import User
+from categories.models import Category
 from .models import Post, Comment
 
 # Create your tests here.
@@ -14,8 +15,10 @@ class BaseTestCase(TestCase):
         cls.today = date.today()
         cls.user = User.objects.create_user("Pingu", "pingu@nootnoot.com", "nootnoot")
         cls.superuser = User.objects.create_superuser("Bob", "example@example.com", "example123")
-        cls.post = Post.objects.create(body="Test Post", poster=cls.superuser)
-        cls.comment = Comment.objects.create(message="Test Comment", commenter=cls.superuser, post=cls.post)
+        cls.post_1 = Post.objects.create(body="Test Post", poster=cls.superuser)
+        cls.post_2 = Post.objects.create(body="Test Post", poster=cls.user)
+        cls.comment = Comment.objects.create(message="Test Comment", commenter=cls.superuser, post=cls.post_1)
+        cls.category = Category.objects.create(name="Test Category")
 
 class TestUnauthorisedViews(BaseTestCase):
     """
@@ -37,19 +40,48 @@ class TestAuthorisedViews(BaseTestCase):
     def setUp(self):
         self.c = APIClient()
         self.c.login(username="Pingu", password="nootnoot")
+        self.post_1 = reverse('individual-post', kwargs={"post_id": 1})
+        self.post_2 = reverse('individual-post', kwargs={"post_id": 2})
 
     def test_auth_index(self):
+        # Logged in user can see all posts
         response = self.c.get(reverse('posts-index'))
         self.assertEqual(response.status_code, 200)
         resp_data = response.data["data"]
-        self.assertEqual(len(resp_data), 1)
+        self.assertEqual(len(resp_data), 2)
 
     def test_auth_create(self):
+        # Can create a post
         test_data = {"body": "New Test Post", "poster": self.user.username, "tags": []}
         response = self.c.post(reverse('posts-create'), test_data, format='json')
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(Post.objects.count(), 2)
-        self.assertEqual(response.data["id"], 2)
+        self.assertEqual(Post.objects.count(), 3)
+        self.assertEqual(response.data["id"], 3)
+
+    def test_auth_post_show(self):
+        # Can find an individual post
+        response = self.c.get(self.post_1)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["data"]["body"], "Test Post")
+
+    def test_auth_post_patch(self):
+        # Can't edit another's post
+        test_data = {"tags": [1]}
+        response = self.c.patch(self.post_1, test_data, format='json')
+        self.assertEqual(response.status_code, 403)
+    
+    def test_auth_post_patch(self):
+        # Can edit their own post
+        test_data = {"tags": [1]}
+        response = self.c.patch(self.post_2, test_data, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["data"]["tags"], [1])
+
+    def test_auth_post_delete(self):
+        # Can delete their own post
+        test_data = {"tags": [1]}
+        response = self.c.delete(self.post_2)
+        self.assertEqual(response.status_code, 204)
 
 class TestAdminViews(BaseTestCase):
     """
@@ -68,7 +100,7 @@ class TestModels(BaseTestCase):
     """
 
     def test_post_str(self):
-        self.assertEqual(str(Post.objects.get()), f"{self.post.body} - {self.post.poster} ({self.today})")
+        self.assertEqual(str(Post.objects.get(pk=1)), f"{self.post_1.body} - {self.post_1.poster} ({self.today})")
 
     def test_comment_str(self):
         self.assertEqual(str(Comment.objects.get()),  f"{self.comment.message} - {self.comment.commenter} ({self.today})")
